1307f78d9bcd7f638edbf5d8a4011935e6d52e65
Walter Franzini <walter.franzini@gmail.com>
2017-12-13 19:07:10 +0100

    adds glibc to the nativeBuildInputs
    It is required since Setup.hs is a dynamic executable, if it's missing
    Setup.hs segfaults during the project build.

--- shell.nix
+++ shell.nix
@@ -2,12 +2,18 @@ with (import <nixpkgs> {});
 
 
 haskell.lib.buildStackProject {
-  # use a version of ghc with integer-simple enabled to avoid using
-  # gmp and related statically linking problems.
+  # NOTE: use a version of ghc with integer-simple enabled to avoid
+  #       using gmp and related statically linking problems.
   ghc = haskell.compiler.integer-simple.ghc822;
 
   name = "stack-nix-static-linking";
   nativeBuildInputs = [
+    # NOTE: the shared variant of glibc is REQUIRED in the
+    #       nativeBuildingInputs since Setup.hs is compiled against
+    #       it.  Failing to add it leads to segfaults from Setup.hs
+    #       while compiling the project.
+    glibc
+
     glibc.static
  ];
 }
